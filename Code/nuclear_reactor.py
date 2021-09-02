28d4f88f7843a2b61a4ee6ab6e1b252266a57b02
coolernato <cmcooling@gmail.com>
2021-09-02 15:46:50 +0100

    Added output states and initial state

--- Code/nuclear_reactor.py
+++ Code/nuclear_reactor.py
@@ -1,6 +1,15 @@
 from input_reader import read_input
 from state import State
+import numpy as np
 
+# Read the input file to form a problem specification
 problem_specification = read_input("Inputs/sample_input1.txt")
 
-initial_state = State(problem_specification, 0)
+# Create the output states of the system. Initially, this is an empty array
+output_states = np.empty(len(problem_specification.output_times), dtype=object)
+
+# Set up the initial state
+output_states[0] = State(problem_specification, 0)
+output_states[0].t_fuel = np.full(problem_specification.n_z, problem_specification.temperature_zero)
+output_states[0].t_cool = np.full(problem_specification.n_z, problem_specification.temperature_zero)
+
